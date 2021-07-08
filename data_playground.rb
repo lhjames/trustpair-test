57696dd68a28b8e8e2d1270bb900a7e1c5493618
Lucille James <lucille.james00@gmail.com>
2021-07-08 11:01:38 +0200

    Delete unused files and change Vat_evaluation init method

--- data_playground.rb
+++
@@ -1,8 +0,0 @@
-require 'JSON'
-
-puts 'Reading the file...'
-file = File.read('data.json')
-data_hash = JSON.parse(file)
-data_hash['id'] = '71'
-File.write('data.json', JSON.dump(data_hash))
-puts 'Rewriting complete!'
--- vat_evaluation_spec.rb
+++ vat_evaluation_spec.rb
@@ -1,10 +1,30 @@
 # frozen_string_literal: true
 
 require File.join(File.dirname(__FILE__), 'vat_evaluation')
+require File.join(File.dirname(__FILE__), 'evaluation')
 
 RSpec.describe VatEvaluation do
 
   describe '#update_vat_company_score' do
-    pending 'implement test'
+    subject! { described_class.new(evaluations).update_vat_company_score }
+
+    context 'API is NOT reachable' do
+
+      context 'when current score is equal or greater than 50' do
+        let(:evaluations) { [Evaluation.new(type: 'SIREN', value: 'IE6388047V', score: 80, state: 'unconfirmed', reason: 'unable_to_reach_api')] }
+
+        it 'should decrease the score of 1 point' do
+          expect(evaluations.first.score).to eq(79)
+        end
+      end
+
+      context 'when current score is lower than 50' do
+        let(:evaluations) { [Evaluation.new(type: 'SIREN', value: 'IE6388047V', score: 30, state: 'unconfirmed', reason: 'unable_to_reach_api')] }
+
+        it 'should decrease the score of 3 points' do
+          expect(evaluations.first.score).to eq(27)
+        end
+      end
+    end
   end
 end
